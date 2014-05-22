0cddbb2a36c1dd2ea80cd2e675efed26ee6a1f16
phoet <phoetmail@googlemail.com>
2014-05-22 10:13:11 -0400

    ruby 1.9.3 (and jruby)

--- lib/partially_useful/partial_renderer.rb
+++ lib/partially_useful/partial_renderer.rb
@@ -1,8 +1,13 @@
 module PartiallyUseful
   module PartialRenderer
+    def self.included(klass)
+      klass.send :alias_method, :original_rails_render, :render
+      klass.send :remove_method, :render
+    end
+
     def render(context, options, block)
       msg = "rendering '#{options[:partial]}' with locals '#{(options[:locals] || {}).keys}'"
-      "<!-- start #{msg}-->\n#{super(context, options, block)}\n<!-- end #{msg}-->\n".html_safe
+      "<!-- start #{msg}-->\n#{original_rails_render(context, options, block)}\n<!-- end #{msg}-->\n".html_safe
     end
   end
 end
