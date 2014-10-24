f3577192f1eee23adace9500b11dcf1497bd5c70
phoet <phoetmail@googlemail.com>
2014-10-24 20:31:12 +0200

    update to rails 4.2.0.beta2

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,11 +3,28 @@ require File.expand_path('../boot', __FILE__)
 require 'action_controller/railtie'
 require 'action_view/railtie'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require "partially_useful"
+require "responders"
 
 module Dummy
   class Application < Rails::Application
     config.partially_useful = true
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
